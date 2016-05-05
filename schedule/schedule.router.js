4ca84bcd7a096527ec1dc603e42aeaed3c06e9cf
Eric <eric.richardson30@gmail.com>
2016-05-05 13:54:49 -0400

    updated files

--- schedule/schedule.router.js
+++ schedule/schedule.router.js
@@ -7,6 +7,24 @@ var Schedules = require('./schedule.schema');
 // API ROUTES
 var apiRouter = express.Router();
 
+apiRouter.post('/', function(req, res) {
+    console.log(req.body);
+    var schedule = new Schedules();
+    
+    schedule.real_name = req.body.real_name;
+    schedule.userName = req.body.userName;
+    schedule.email = req.body.email;
+    schedule.userId = req.body.userId;
+    schedule.date = req.body.date;
+    schedule.channel = req.body.channel;
+    schedule.expecting = req.body.expecting;
+    
+    schedule.save(function(err, schedule) {
+        if (err) res.send(err);
+        res.json({message: 'Schedule was created'});
+    });
+});
+
 apiRouter.get('/', function(req, res) {
     console.log("get all schedules");
     Schedules.find({}, function(err, schedules) {
@@ -27,23 +45,6 @@ apiRouter.get('/now', function(req, res) {
     //         if(err) res.send(err);
     //         res.json(results);
     //     });
-});
-
-apiRouter.post('/', function(req, res) {
-    var schedule = new Schedules();
-    console.log(req.body);
-    schedule.real_name = req.body.real_name;
-    schedule.userName = req.body.userName;
-    schedule.email = req.body.email;
-    schedule.userId = req.body.userId;
-    schedule.date = req.body.date;
-    schedule.channel = req.body.channel;
-    schedule.expecting = req.body.expecting;
-    
-    schedule.save(function(err, schedule) {
-        if (err) res.send(err);
-        res.json({message: 'Schedule was created'});
-    });
 });
 
 module.exports = apiRouter;
