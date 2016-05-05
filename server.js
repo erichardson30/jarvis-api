c9a906bf9713c495a550d10f4fdb5d990259186b
Eric <eric.richardson30@gmail.com>
2016-05-05 13:52:41 -0400

    allowed post

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 // Configure app to handle CORS requests
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \ Authorization');
   next();
 });
